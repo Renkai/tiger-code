0ba44f2b36968305bd25fde66ec6871aababf9e7
Renkai Ge <renkaige@tubi.tv>
2024-03-23 12:06:09 +0800

    Wrap spaces (#1)
    * On branch wrap_spaces
     Changes to be committed:
    	new file:   lua/wrap_spaces.lua
    	modified:   rime.lua
    	modified:   tiger.custom.yaml
    	modified:   tigress.custom.yaml
    	modified:   tigress.schema.yaml
    
    * Changes to be committed:
    	modified:   lua/wrap_spaces.lua
    
    * Changes to be committed:
    	modified:   lua/wrap_spaces.lua
    
    ---------
    
    Co-authored-by: gaelookair@gmail.com <gerenkai@gerenkaideMac-mini.local>

---
+++ lua/wrap_spaces.lua
@@ -0,0 +1,12 @@
+local function translator(input, seg)
+      if (input:sub(1, 1) == '\'') then
+         local remove_head = string.sub(input, 2)
+         local with_spaces = " " .. remove_head .. " "
+         local with_grave_accents = " `" .. remove_head .. "` "
+         yield(Candidate("ASCII", seg.start, seg._end, with_spaces, "空格"))
+         yield(Candidate("ASCII", seg.start, seg._end, with_grave_accents, "反引"))
+         yield(Candidate("ASCII", seg.start, seg._end, remove_head, "原始"))
+      end
+ end
+ 
+ return translator
